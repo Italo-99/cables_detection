dc1119bee7c13544ffd155f42d288fb59e0975e1
Italo <255778@studenti.unimore.it>
2024-02-05 17:46:14 +0100

    Bug fixing

--- scripts/fastdlo_server.py
+++ scripts/fastdlo_server.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 
-import rospy
-from sensor_msgs.msg import Image
-from fiducial_msgs.msg import FiducialArray
-from cables_detection.srv import Cable2D_Poses
+""" CENTROID ARUCO SERVER
+The following code has the only aim to compute the centroid of
+a fiducial marker detected.
+"""
+
+from cables_detection.srv import Cables2D_Poses
 from cv_bridge import CvBridge
 import cv2
+from fiducial_msgs.msg import FiducialArray
+from geometry_msgs.msg import PoseArray,Pose
+import numpy as np
+import rospy
+from sensor_msgs.msg import Image
+
 
 class Centroid_ArucoServer:
 
@@ -13,12 +21,11 @@ class Centroid_ArucoServer:
 
         # Initialize global class variables
         self.image = FiducialArray()
-        self.centroids = []
 
         # Initialize ROS node
         rospy.init_node('centroid_arucoserver')
         rospy.Subscriber('/fiducial_vertices', FiducialArray, self.fiducial_vertices_callback)
-        rospy.Service('centroid_aruco', Cable2D_Poses, self.handle_centroid_aruco)
+        rospy.Service('centroid_aruco', Cables2D_Poses, self.handle_centroid_aruco)
         rospy.loginfo("Ready to compute centroids of the fiducial vertices.")
 
         # Run ROS spinner
@@ -27,20 +34,26 @@ class Centroid_ArucoServer:
             spin_rate.sleep()
 
     def fiducial_vertices_callback(self,msg):
-        
+
         self.image = msg
 
     def handle_centroid_aruco(self,req):
 
+        centroids = PoseArray()
+
         for fiducial in self.image.fiducials:
+
+            xc = (fiducial.x0+fiducial.x1+fiducial.x2+fiducial.x3)/4
+            yc = (fiducial.y0+fiducial.y1+fiducial.y2+fiducial.y3)/4
+            new_point = Pose()
+            new_point.position.x = xc
+            new_point.position.y = yc
+            centroids.poses.append(new_point)
             fiducial_id = fiducial.fiducial_id
-            xc = (fiducial.x0+fiducial.x1+fiducial.x2+fiducial.x3+fiducial.x4)/4
-            yc = (fiducial.y0+fiducial.y1+fiducial.y2+fiducial.y3+fiducial.y4)/4
-            self.centroids.append([xc,yc])
-            rospy.loginfo("Fiducial ID: %d, Centroid: %s", fiducial_id, self.centroids[-1])
+            rospy.loginfo("Fiducial ID: %d\nCentroid: %s", fiducial_id, centroids.poses[-1].position)
 
-        return self.centroids
-
+        return centroids
+    
 if __name__ == "__main__":
     
     Centroid_ArucoServer()
